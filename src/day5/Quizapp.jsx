1805f8e6ac6846c40479bb4b213e7a741d15e7b9
Abishek1006 <abishek.2201006@srec.ac.in>
2025-08-05 22:49:10 +0530

    day5

---
+++ src/day5/Quizapp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Quizapp() {
+  return (
+    <div>
+        <div>
+
+            <h1>Quiz App</h1>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
